57695b782f4c5478cf5290d259c10c7d000dbe72
ryanDev <ryan_toh@outlook.sg>
2026-07-17 15:21:51 +0800

    Remove View, Tab bar to improve Editor UI/UX

--- js/components/tab-bar/dom.js
+++
@@ -1,9 +0,0 @@
-export const tabBarDom = {}
-
-export function initTabBarDomElements() {
-    Object.assign(tabBarDom, {
-        tabBar: document.getElementById('tab-bar'),
-        tabView: document.getElementById('tab-view'),
-        tabEdit: document.getElementById('tab-edit'),
-    })
-}
